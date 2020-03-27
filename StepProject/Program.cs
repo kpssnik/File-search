5f2ddb4bd51bd9848d8f05034ef5a56151e63098
kpssnik <kpssdeveloper@gmail.com>
2020-03-27 22:52:29 +0200

    Код перемещён в ветку master

--- StepProject/Program.cs
+++ StepProject/Program.cs
@@ -10,60 +10,14 @@ namespace StepProject
 {
     class Program
     {
-        static List<string> foundFiles = new List<string>();
+       
         static void Main(string[] args)
         {
 
-            if (args.Length <= 0)
-            {
-                PrintMessage("Insert filename to search", ConsoleColor.Red);
-            }
-            else
-            {
-                Console.WriteLine("Search started...");
-                foreach (var drive in DriveInfo.GetDrives())
-                {
-                    new Thread(() =>
-                    {
-                        KpssSearch($"{drive}\\", args[0]);
-                    }).Start();
-                }
-
-                //foreach (var a in foundFiles) PrintMessage(a, ConsoleColor.Green);            
-            }
-            Console.ReadLine();
+           
         }
 
 
-        static void KpssSearch(string dir, string pattern)
-        {
-            for (int i = 0; i < Directory.GetDirectories(dir).Length; i++)
-            {
-                try
-                {
-                    foreach (string f in Directory.GetFiles(Directory.GetDirectories(dir)[i]))
-                    {
-                        if (f.EndsWith($"\\{pattern}"))
-                        {
-                            foundFiles.Add(f);
-                            PrintMessage(f, ConsoleColor.Green);
-                        }
-                    }
-                    KpssSearch(Directory.GetDirectories(dir)[i], pattern);
-                }
-                catch (Exception ex)
-                {
-                    //PrintMessage(ex.Message, ConsoleColor.Red);
-                    continue;
-                }
-            }
-        }
-
-        static void PrintMessage(string msg, ConsoleColor color)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(msg);
-            Console.ResetColor();
-        }
+       
     }
 }
